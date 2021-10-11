8ed706f7d77ae0fd02c98c8bae613b0dcde3ba68
elieh <elie.hallermeier@gmail.com>
2021-10-11 19:48:31 +0300

    Initial commit

---
+++ app/src/main/java/com/octorbit/hex/HotseatActivity.java
@@ -0,0 +1,14 @@
+    package com.octorbit.hex;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HotseatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hotseat);
+    }
+}
---
+++ app/src/main/java/com/octorbit/hex/SettingNameActivity.java
@@ -0,0 +1,14 @@
+package com.octorbit.hex;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SettingNameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting_name);
+    }
+}
